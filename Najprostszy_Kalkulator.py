da21f748971249cb402eeb6271a9fb693f48415c
Wojciech Wawrzyszko <wuwu1978@o2.pl>
2024-09-30 16:20:33 +0200

    Wersja 1.01. Kalkulator działa w pętli.

--- Najprostszy_Kalkulator.py
+++ Najprostszy_Kalkulator.py
@@ -1,5 +1,9 @@
 # NAJPROSTSZY KALKULATOR W PYTHONIE
-# Wersja: 1.0
+# Wersja: 1.01
 
-x = eval(input("Tu kalkulator. Podaj liczby i wymagane działanie: "))
-print(x)
+while True:
+    print("")
+    print("KALKULATOR, Wersja: 1.01")
+    print("(Wpisanie litery oznacza wyjście z programu)")
+    x = eval(input("Tu kalkulator. Podaj liczby i wymagane działanie: "))
+    print(x)
